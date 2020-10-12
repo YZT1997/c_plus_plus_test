75ff97798a89e2586e28b3664e4db2841919f11a
YZT1997 <yangzt_0943@163.com>
2020-10-12 10:30:33 +0800

    通过友元函数 定义 << 运算符重载

--- test_laptop/8_1.cpp
+++ test_laptop/8_1.cpp
@@ -9,7 +9,8 @@ public:
     Complex(double r = 0.0, double i = 0.0) : real(r), imag(i){}
     Complex operator+ (const Complex &c) const;
     Complex operator- (const Complex &c) const;
-    void display() const;
+    friend ostream &operator << (ostream &out, const Complex &c); // 运算符 << reload with friend function
+//    void display() const;
 
 private:
     double real;
@@ -24,20 +25,21 @@ Complex Complex::operator-(const Complex &c) const {
     return Complex(real - c.real, imag - c.imag);
 }
 
-void Complex::display() const {
-    cout<<"("<<real<<", "<<imag<<"i)"<<endl;
+//void Complex::display() const {
+//    cout<<"("<<real<<", "<<imag<<"i)"<<endl;
+//}
+
+// 运算符 << reload
+ostream &operator << (ostream &out, const Complex &c){
+    out<<"("<<c.real<<", "<<c.imag<<")"<<endl;
 }
 
 int main(){
     Complex c1(10, 200), c2(0, 19), c3;
-    cout<<"c1 = ";
-    c1.display();
-    cout<<"c2 = ";
-    c2.display();
+    cout<<"c1 = "<<c1;
+    cout<<"c2 = "<<c2;
 
-    cout<<"c1 + c2 = ";
     c3 = c1 + c2;
-    c3.display();
-
+    cout<<"c1 + c2 = "<<c3;
     return 0;
 }
