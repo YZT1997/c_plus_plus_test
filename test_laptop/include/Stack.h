2725da6ed2ae465fdc3f9997265d753cb33400b1
YZT1997 <yangzt_0943@163.com>
2020-10-27 09:19:08 +0800

    栈

---
+++ test_laptop/include/Stack.h
@@ -0,0 +1,8 @@
+//
+// Created by yang zhengtao on 2020/10/27.
+//
+
+#ifndef TEST_LAPTOP_STACK_H
+#define TEST_LAPTOP_STACK_H
+
+#endif //TEST_LAPTOP_STACK_H
---
+++ test_laptop/test.cpp
@@ -0,0 +1,13 @@
+//
+// Created by yang zhengtao on 2020/10/23.
+//
+#include "iostream"
+using namespace std;
+
+
+int main(){
+    uint16_t a = 0xff;
+    uint16_t b = a + 0xff;
+    cout<<hex<<a<<" "<<hex<<b<<endl;
+    return 0;
+}
