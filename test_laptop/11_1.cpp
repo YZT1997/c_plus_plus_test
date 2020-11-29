900544f59f083b9455df71da375f33833455749c
YZT1997 <yangzt_0943@163.com>
2020-11-29 19:50:54 +0800

    setw使用

--- test_laptop/11_1.cpp
+++ test_laptop/11_1.cpp
@@ -2,14 +2,15 @@
 // Created by yang zhengtao on 2020/11/29.
 //
 #include "iostream"
+#include "iomanip"
+#include "string"
 using namespace std;
 
 int main(){
     double values[] = {1.23, 35.36, 653.7, 4358.24};
+    string names[] = {"Hello", "My", "Pleasure", "Last"};
     for (int i = 0; i < 4; ++i) {
-        cout.width(10);
-        cout.fill('-');
-        cout<<values[i]<<endl;
+        cout<<setw(8)<<names[i]<<setw(10)<<values[i]<<endl;
     }
     return 0;
 }
